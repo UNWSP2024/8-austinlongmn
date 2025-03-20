063dc9cdfe1fc2f7cf7713bcbde755a978a2fb8c
Austin <137583592+austinlongmn@users.noreply.github.com>
2025-03-19 20:59:28 -0400

    Wrote programs

--- program_3.py
+++ program_3.py
@@ -1,8 +1,159 @@
+# Programmer: Austin Long
+# Date: 2025-03-19
+# Program: Capital Quiz
+
 # Program #3: Capital Quiz
-# Write a program that creates a dictionary containing the U.S. states as keys, 
-# and their capitals as values.  
-# The program should then randomly quiz the user by displaying the name of a state 
-# and asking the user to enter the state's capital.  
-# The program should count of the number of correct and incorrect responses.  
-# (You could alternatively use another country and provinces, 
+# Write a program that creates a dictionary containing the U.S. states as keys,
+# and their capitals as values.
+# The program should then randomly quiz the user by displaying the name of a state
+# and asking the user to enter the state's capital.
+# The program should count of the number of correct and incorrect responses.
+# (You could alternatively use another country and provinces,
 # or countries of the world and capitals).
+
+import random
+import os
+import sys
+from typing import Any, Callable
+
+# I copied the data off a site and formatted it with Vim. Quite the pleasure!
+CAPITALS = {
+    "ALABAMA": "Montgomery",
+    "Alaska": "Juneau",
+    "Arizona": "Phoenix",
+    "Arkansas": "Little Rock",
+    "California": "Sacramento",
+    "Colorado": "Denver",
+    "Connecticut": "Hartford",
+    "Delaware": "Dover",
+    "Florida": "Tallahassee",
+    "Georgia": "Atlanta",
+    "Hawaii": "Honolulu",
+    "Idaho": "Boise",
+    "Illinois": "Springfield",
+    "Indiana": "Indianapolis",
+    "Iowa": "Des Moines",
+    "Kansas": "Topeka",
+    "Kentucky": "Frankfort",
+    "Louisiana": "Baton Rouge",
+    "Maine": "Augusta",
+    "Maryland": "Annapolis",
+    "Massachusetts": "Boston",
+    "Michigan": "Lansing",
+    "Minnesota": "Saint Paul",
+    "Mississippi": "Jackson",
+    "Missouri": "Jefferson City",
+    "Montana": "Helena",
+    "Nebraska": "Lincoln",
+    "Nevada": "Carson City",
+    "New Hampshire": "Concord",
+    "New Jersey": "Trenton",
+    "New Mexico": "Santa Fe",
+    "New York": "Albany",
+    "North Carolina": "Raleigh",
+    "North Dakota": "Bismarck",
+    "Ohio": "Columbus",
+    "Oklahoma": "Oklahoma City",
+    "Oregon": "Salem",
+    "Pennsylvania": "Harrisburg",
+    "Rhode Island": "Providence",
+    "South Carolina": "Columbia",
+    "South Dakota": "Pierre",
+    "Tennessee": "Nashville",
+    "Texas": "Austin",
+    "Utah": "Salt Lake City",
+    "Vermont": "Montpelier",
+    "Virginia": "Richmond",
+    "Washington": "Olympia",
+    "West Virginia": "Charleston",
+    "Wisconsin": "Madison",
+    "WYOMING": "Cheyenne",
+}
+
+# These were copied from last week's programs
+
+
+# Gets input where the user is not expected to quit.
+def get_input(
+    prompt: str,
+    conversion: Callable[[str], Any],
+    err_message: str = "Invalid input.",
+    validation: Callable[[Any], bool] = lambda _: True,
+) -> Any:
+    return interactive_get_input(prompt, conversion, err_message, validation, None)[1]
+
+
+# Gets input. If the user inputs terminator, then the function signals this to the caller.
+def interactive_get_input(
+    prompt: str,
+    conversion: Callable[[str], Any],
+    err_message: str = "Invalid input.",
+    validation: Callable[[Any], bool] = lambda _: True,
+    terminator: str | None = "",
+) -> tuple[bool, Any]:
+    try:
+        # Gets input from user. If the program isn't running in interactive
+        # mode, this skips prompting.
+        user_input = input(prompt if os.isatty(0) else "")
+
+        # If terminator found, signal to caller.
+        if terminator != None and user_input == terminator:
+            return (True, None)
+
+        # convert input
+        converted = conversion(user_input)
+
+        # Check input validity
+        if validation(converted):
+            # Return successful value
+            return (False, converted)
+    except ValueError:
+        pass
+
+    # Retry input if error
+    print(err_message, file=sys.stderr)
+    return interactive_get_input(
+        prompt, conversion, err_message, validation, terminator
+    )
+
+
+def main():
+    capitals_keys = list(CAPITALS.keys())
+
+    num_correct = 0
+    num_incorrect = 0
+
+    while True:
+        state = random.choice(capitals_keys)
+
+        (done, response) = interactive_get_input(
+            f"Enter the capital for {state} (leave empty if done) ",
+            str,
+        )
+
+        if done:
+            break
+
+        normalized_response = response.strip().upper()
+        capital = CAPITALS[state]
+        normalized_capital = capital.upper()
+
+        if normalized_response == normalized_capital:
+            print("Correct!")
+            num_correct += 1
+        else:
+            print(f"Incorrect. The correct answer was {capital}.")
+            num_incorrect += 1
+
+    total = num_correct + num_incorrect
+
+    if total == 0:
+        print("Aww, come on! At least try!")
+        return  # avoid division by zero
+
+    print("Thanks for playing!")
+    print(f"Your final score is {num_correct}/{total} ({num_correct/total:.2%}).")
+
+
+if __name__ == "__main__":
+    main()
--- program_5.py
+++ program_5.py
@@ -1,5 +1,78 @@
+# Programmer: Austin Long
+# Date: 2025-03-19
+# Program: Course Info
+
 # Program #5: Course Info
-# Write a program that has the user input a bunch of course ID and course name pairs.  
-# For example a course ID could be "COS 2005" and the course name could be "Python Programming."   
-# Then ask the user for a subject (like "COS"). 
+# Write a program that has the user input a bunch of course ID and course name pairs.
+# For example a course ID could be "COS 2005" and the course name could be "Python Programming."
+# Then ask the user for a subject (like "COS").
 # Finally, the program will display the ID and name of all the courses having that subject.
+
+# Pseudocode
+
+# Create dictionary
+# While user enters input
+#   Get course id and course description from user
+#   Get category from course ID
+#   Put course id and description into dictionary under category
+# Get course IDs for subject
+# For course id
+#   Get course name
+#   Print course ID and name
+
+import re
+from program_3 import get_input, interactive_get_input
+
+
+def input_course(directory):
+    def validate_course_id(course_id):
+        return not not re.match(r"^[A-Za-z]{3}[0-9]{4}$", course_id)
+
+    (done, course_id) = interactive_get_input(
+        "Enter the course ID (empty for done): ",
+        str,
+        "That is an invalid course ID. Course IDs are in the format ABC1234.",
+        validate_course_id,
+    )
+
+    if done:
+        return False
+
+    norm_course_id = course_id.upper().strip()
+
+    course_category = re.match(r"^[A-Z]{3}", norm_course_id).group(0)
+
+    if directory.get(course_category) == None:
+        directory[course_category] = []
+
+    course_info = input("Enter the course information: ")
+
+    directory[course_category].append(norm_course_id + ": " + course_info)
+
+    return True
+
+
+def main():
+    course_directory = {}
+
+    while input_course(course_directory):
+        continue
+
+    def input_conversion(user_input):
+        try:
+            return course_directory[user_input.strip().upper()]
+        except KeyError:
+            raise ValueError()
+
+    courses = get_input(
+        f"Enter a category: ",
+        input_conversion,
+        err_message=f"That category is not in the directory.",
+    )
+
+    for course in courses:
+        print(course)
+
+
+if __name__ == "__main__":
+    main()
